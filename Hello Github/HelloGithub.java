942ef63f6e11c15a18f1cbc37ad6330770d10fca
Aryan Jain <aryanjn807@gmail.com>
2019-03-19 01:41:35 +0530

    Added #1

---
+++ Hello Github/HelloGithub.java
@@ -0,0 +1,20 @@
+package java_project;
+
+//Unlike C++ which is partially object oriented, 
+//JAVA is FULLY object oriented programming language.
+//That means we are going to write each & everything inside classes.
+
+
+//Syntax of class:
+public class Date 
+{
+	//#1. The first thing your computer does is it looks for a method call main.
+	//#2. You program starts from main method.
+	
+	//Syntax of main method is given below : 
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Github!!");
+	}
+}
+
