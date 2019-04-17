9be73b01dfe27162abf83c45bd35aecfeff6f47b
Aryan Jain <aryanjn807@gmail.com>
2019-04-17 11:02:20 +0530

    Added creating own package

---
+++ #9.Creating own package/creating_own_package.java
@@ -0,0 +1,19 @@
+//CREATING PACKAGE :: 
+
+package packagename; //packagename can be anything you wish
+
+// classes and methods inside packagename :
+
+public class pack   
+{
+	public String method1()
+	{
+		return "Method 1";
+	}
+	
+	public String method2()
+	{
+		return "Method 2";
+	}
+}
+
---
+++ #9.Creating own package/using_created_package.java
@@ -0,0 +1,19 @@
+//USING PACKAGE THAT IS CREATED : 
+
+// import all the methods from packagename package
+import packagename.*;
+
+class Test
+{	
+	public static void main(String[] args) 
+	{
+		// creating object of class present inside pack class
+		
+		pack o1 = new pack();
+		System.out.println(o1.method1());
+		
+		pack o2 = new pack();
+		System.out.println(o2.method2());
+	}
+}
+
